5d2f9e690014020e83d0f23cd9b1027884291bb8
Etopia1 <jolaetopia81@gmail.com>
2025-07-31 03:21:15 +0100

    new bcommiot

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const __dirname = path.resolve();
 
 // const app = express()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://purlse-chat-front.vercel.app",
     credentials: true
 }))
 
